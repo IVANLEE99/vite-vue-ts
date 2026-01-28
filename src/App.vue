75532a29576b9f6f732ac65f414bf721de6871e8
youngs <lixiang@popicorns.com>
2026-01-28 10:28:14 +0800

    feat:init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello Vue 3 + TypeScript + Vite</h1>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+console.log("vite.config.ts loaded ✅");
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
